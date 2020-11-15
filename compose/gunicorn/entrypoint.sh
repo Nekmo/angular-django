57a3b5869f1c833c4500d5085c725429ab047ed8
Nekmo <contacto@nekmo.com>
2020-11-16 00:15:28 +0100

    Issue #4: Serve demo project

--- compose/gunicorn/entrypoint.sh
+++ compose/gunicorn/entrypoint.sh
@@ -7,5 +7,6 @@ set -o nounset
 python manage.py collectstatic --no-input
 python manage.py migrate --no-input
 python manage.py import_pokedex
+python manage.py createsuperuser --username demo --email demo@nekmo.org --no-input || true
 
 exec "$@"
