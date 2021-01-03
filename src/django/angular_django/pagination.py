61764f931e98bca961d3c6474b958267b8d346dd
Nekmo <contacto@nekmo.com>
2021-01-04 00:32:18 +0100

    Issue #20: Additional info in lists

--- src/django/angular_django/pagination.py
+++ src/django/angular_django/pagination.py
@@ -14,6 +14,14 @@ class AngularDjangoPageNumberPagination(PageNumberPagination):
             ('results', data)
         ]))
 
+    def get_paginated_response_schema(self, schema):
+        response_schema = super().get_paginated_response_schema(schema)
+        response_schema['properties']['page_size'] = {
+            'type': 'integer',
+            'example': 10,
+        }
+        return response_schema
+
 
 class StandardResultsSetPagination(AngularDjangoPageNumberPagination):
     page_size = 10
