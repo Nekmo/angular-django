02d84a574c1c35d3939a95dc8b1cb5cca5b29815
Nekmo <contacto@nekmo.com>
2021-01-06 21:44:43 +0100

    Issue #24: Upgrade angular to Angular 11

--- src/angular/projects/angular-django/material/src/angular-django-material.module.ts
+++ src/angular/projects/angular-django/material/src/angular-django-material.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import {AngularDjangoMaterialTableComponent} from './angular-django-material-table/angular-django-material-table.component';
-import {AngularDjangoModule} from '../../src/lib/angular-django.module';
+// import {AngularDjangoModule} from '../../src/lib/angular-django.module';
 // Uncomment the following line for development
 // import { AngularDjangoModule } from '../../src/public-api';
 
