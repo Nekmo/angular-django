050a6db45a39205bb339b0e07cc1e240b4df403d
Nekmo <contacto@nekmo.com>
2021-01-06 22:27:23 +0100

    Issue #8: Material sub-package

--- src/angular/projects/angular-django/material/src/angular-django-material.module.ts
+++ src/angular/projects/angular-django/material/src/angular-django-material.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import {AngularDjangoMaterialTableComponent} from './angular-django-material-table/angular-django-material-table.component';
-// import {AngularDjangoModule} from '../../src/lib/angular-django.module';
-// Uncomment the following line for development
-// import { AngularDjangoModule } from '../../src/public-api';
+import {AngularDjangoModule} from 'angular-django';
 
 
 @NgModule({
   imports: [
-    // AngularDjangoModule,
+    AngularDjangoModule,
   ],
   declarations: [
     AngularDjangoMaterialTableComponent,
