05dc0812ac2add49043a7030e6c10c19499ea0a8
Nekmo <contacto@nekmo.com>
2021-07-30 14:45:30 +0200

    TODO comment.

--- src/angular/projects/angular-django/src/lib/utils.ts
+++ src/angular/projects/angular-django/src/lib/utils.ts
@@ -24,7 +24,7 @@ export function getNestedDictionary(dictionary: Dictionary<any>, nestedKey: stri
         throw new Error(`Invalid item ${subFieldName} on ${nestedKey} query`);
     }
     if (dictionary.type === 'nested object') {
-        dictionary = dictionary.children;
+        dictionary = dictionary.children;  // TODO: al devolverse dictionary.children no se sabe el tipo.
     }
   });
   return dictionary;
